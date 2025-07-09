3ffea5ca41bd66c2576e42cd35da47bd3f5b5b53
Luthfi Abdillah <luthfiabd.14@gmail.com>
2025-07-10 03:15:24 +0700

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Atur Carbon ke locale Indonesia
         Carbon::setLocale('id');
         
